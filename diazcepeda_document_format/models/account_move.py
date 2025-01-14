3803c7f47750ef042507185f7260cff9898636c2
dani <dani.xtendoo@gmail.com>
2025-01-14 08:57:38 +0100

    [MOD] diazcepeda_document_format, añadido gasto de gestio en el footer, en caso de que lo llevase y oculto de las lineas

--- diazcepeda_document_format/models/account_move.py
+++ diazcepeda_document_format/models/account_move.py
@@ -4,12 +4,8 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def _get_invoice_lines_to_report(self, lines):
-        print("*"*80)
         invoice_line = lines
-        print("invoice_line", invoice_line)
         filtered_lines = invoice_line.filtered(lambda line: line.product_id.name != 'Gasto de gestión')
-        print("filtered_lines", filtered_lines)
-        print("*"*80)
         return filtered_lines
 
     def _is_gasto_gestion(self):
