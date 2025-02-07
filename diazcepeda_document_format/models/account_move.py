8ef28652141e5c0120e2fd9168bce67f89a6b265
dani <dani.xtendoo@gmail.com>
2025-02-07 11:31:40 +0100

    añadido gasto de gestion en preimpresa

--- diazcepeda_document_format/models/account_move.py
+++ diazcepeda_document_format/models/account_move.py
@@ -4,13 +4,14 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def _get_invoice_lines_to_report(self, lines):
-        print("*"*80)
         invoice_line = lines
-        print("invoice_line", invoice_line)
         filtered_lines = invoice_line.filtered(lambda line: line.product_id.name != 'Gasto de gestión')
-        print("filtered_lines", filtered_lines)
-        print("*"*80)
         return filtered_lines
 
     def _is_gasto_gestion(self):
         return self.invoice_line_ids.filtered(lambda line: line.product_id.name == 'Gasto de gestión').exists()
+
+    def _get_footer_data(self):
+        param_obj = self.env['ir.config_parameter'].sudo()
+        footer_data = param_obj.get_param('footer_data', default='')
+        return footer_data.replace('\n', '<br/>')
