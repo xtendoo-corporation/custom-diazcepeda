8e5ee4db902254ed0aed8aae6d9c13bd88784c04
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-03-26 12:15:42 +0100

    sync

--- diazcepeda_export_xls_contability/wizard/xls_export.py
+++ diazcepeda_export_xls_contability/wizard/xls_export.py
@@ -175,13 +175,13 @@ class DiazCepedaExportXLSContability(models.TransientModel):
 
             # Escribo los datos en el excel
             worksheet.write(row_num, 0, str(invoice.invoice_date.year))  # 'Serie',
-            worksheet.write(row_num, 1, invoice.name.replace("/","")) # 'Factura',
+            worksheet.write(row_num, 1, invoice.name.replace("FAC/","")) # 'Factura',
             worksheet.write(row_num, 2, str(invoice.invoice_date.day) + "/" + str(invoice.invoice_date.month) + "/" + str(invoice.invoice_date.year))  # 'Fecha',
             worksheet.write(row_num, 3, str(invoice.invoice_date.day) + "/" + str(invoice.invoice_date.month) + "/" + str(invoice.invoice_date.year))  # 'FechaOperacion',
             worksheet.write(row_num, 4, "")  # 'CodigoCuenta',mu
-            worksheet.write(row_num, 5, invoice.partner_id.vat)  # 'CIFEUROPEO',
+            worksheet.write(row_num, 5, invoice.partner_id.vat.repalace("ES",""))  # 'CIFEUROPEO',
             worksheet.write(row_num, 6, invoice.partner_id.name)  # 'Cliente',
-            worksheet.write(row_num, 7, "FRA. Nº. " + invoice.name + " - " + invoice.partner_id.name)  # 'Comentario',
+            worksheet.write(row_num, 7, "FRA. Nº. " + invoice.name.replace("/","") + " - " + invoice.partner_id.name)  # 'Comentario',
             worksheet.write(row_num, 8, "")  # 'Contrapartida',
             worksheet.write(row_num, 9, "")  # 'Cod.Transacion',
             worksheet.write(row_num, 10, "") # 'ClaveOperaciónFact',
