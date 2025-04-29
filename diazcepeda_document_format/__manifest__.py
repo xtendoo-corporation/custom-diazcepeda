6ae825dc9cd4b7c60578fa06246f50d59f9476e1
PepeDev26 <pepeaguilaralanis@hotmail.com>
2025-04-29 13:59:38 +0200

    Fixed vacation and bankaccounts

--- diazcepeda_document_format/__manifest__.py
+++ diazcepeda_document_format/__manifest__.py
@@ -20,6 +20,7 @@
         "views/invoice/report_invoice_document_preprinted.xml",
         "views/stock_picking/report_delivery_document.xml",
         "views/layout/external_layout_striped.xml",
+        "views/layout/external_layout_striped_vacation.xml",
         "views/interface/product_template_view.xml",
         "views/sale/report_saleorder_document_with_vat.xml",
         "views/sale/report_saleorder_document_without_prices.xml",
--- diazcepeda_document_format/models/__init__.py
+++ diazcepeda_document_format/models/__init__.py
@@ -2,3 +2,4 @@ from . import sale_order
 from . import account_move
 from . import product_template
 from . import account_move_line
+from . import vacation_activator
