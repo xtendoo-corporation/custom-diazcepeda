b6de77c75ea154a5b941d26e46cdf4c300e8ff8d
dani <dani.xtendoo@gmail.com>
2026-04-21 12:55:07 +0200

    cambiado menu de exportacion schweppes

--- diazcepeda_schweppes_iris/__manifest__.py
+++ diazcepeda_schweppes_iris/__manifest__.py
@@ -6,7 +6,14 @@
     'description': 'Módulo de generación de fichero .txt posicional',
     'author': 'Guillermo Barcena Lopez',
     'website': 'https://xtendoo.es',
-    'depends': ['base', 'sale', 'account', 'stock', 'mail'],
+    'depends': [
+        'base',
+        'sale',
+        'account',
+        'stock',
+        'mail',
+        'diazcepeda_administration',
+    ],
     'data': [
         'security/ir.model.access.csv',
         'data/ir_sequence_data.xml',
